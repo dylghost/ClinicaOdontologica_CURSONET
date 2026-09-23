bea03f23b0518f913cf93379c22c411c1c03fa95
Dylan <dylancito.cabrera15@gmail.com>
2026-09-22 21:52:01 -0500

    Creacion de tabla debil con foreign key :)

---
+++ ClinicaOdontologica.Modelos/HistorialMedico.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicaOdontologica.Modelos
+{
+    [Table("historialesmedicos")]
+    public class HistorialMedico
+    {
+        [Key]
+
+        [Required]
+        public int idHistorialMedico { get; set; }
+        [Required]
+        public string alergias { get; set; }
+        public string enfermedadesPrevias { get; set; }
+        public string tipoSangre { get; set; }
+        [ForeignKey("")]
+        [Column("id_paciente")]
+       
+        public int idPaciente { get; set; }
+        public Paciente? paciente { get; set; }
+    }
+}
