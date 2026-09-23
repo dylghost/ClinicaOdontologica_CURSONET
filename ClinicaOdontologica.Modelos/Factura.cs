d62997aff0d1b2ec649b1225b02a3362ea379ac7
Dylan <dylancito.cabrera15@gmail.com>
2026-09-22 23:23:12 -0500

    Finalizada segunda creación de tablas debiles con una clave foranea

---
+++ ClinicaOdontologica.Modelos/Factura.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicaOdontologica.Modelos
+{
+    [Table("facturas")]
+    public class Factura
+    {
+        [Key]
+        [Column("id_factura",TypeName ="Serial")]
+        [Required]
+
+        public int idFactura { get; set; }
+        [Column("fecha_emision",TypeName ="Time Zone")]
+        [Required]
+        public DateTime fechaEmision { get; set; }
+        [Column("subtotal", TypeName = "Decimal(10,2)")]
+        [Required]
+        public decimal subTotal { get; set; }
+        
+        [Column(TypeName = "Decimal(10,2)")]
+        [Required]
+        public decimal impuestos { get; set; }
+        [Column(TypeName = "Decimal(10,2)")]
+        [Required]
+        public decimal total { get; set; }
+        [Column("estado_pago")]
+        public string estadoPago { get; set; }
+        [ForeignKey("Cita")]
+        [Column("id_cita")]
+        public int idCita { get; set; } 
+        public Cita cita { get; set; }
+    }
+}
---
+++ ClinicaOdontologica.Modelos/Receta.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicaOdontologica.Modelos
+{
+    [Table("recetas")]
+    public class Receta
+    {
+        [Key]
+        [Column("id_receta", TypeName = "Serial")]
+        [Required]
+        public int idReceta { get; set; }
+        [Column("fecha_emision")]
+        [Required]
+        public DateTime fechaEmision { get; set; }
+        [Required]
+        public string indicaciones { get; set; }
+        [ForeignKey("Cita")]
+        [Column("id_cita")]
+        [Required]
+        public int idCita { get; set; }
+        public Cita? cita { get; set; } 
+        
+
+    }
+}
