c259d9bb5b68f1f524117bd3c3b9557047b2bf77
Dylan <dylancito.cabrera15@gmail.com>
2026-09-22 23:30:49 -0500

    Culminada 3 fase de tablas debiles con dos claves foraneas

--- ClinicaOdontologica.Modelos/Cita.cs
+++ ClinicaOdontologica.Modelos/Cita.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,27 @@ namespace ClinicaOdontologica.Modelos
 {
     public  class Cita
     {
+        [Key]
+        [Column("id_detalle_cita", TypeName ="Serial")]
+        [Required]
+        public int idDetalleCita { get; set; }
+        [ForeignKey("Cita")]
+        [Column("id_cita")]
+        [Required]
+        public int idCita { get; set; }
+        [ForeignKey("Tratamiento")]
+        [Column("id_tratamiento")]
+        [Required]
+        public int idTratamiento { get; set; }
+        [Column("costo_aplicado", TypeName = "Decimal(10,2)")]
+        [Required]
+        public decimal costoAplicado { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string observaciones { get; set; }
 
+        //Objetos de navegacion
+        public Cita? cita { get; set; }
+        public Tratamiento tratamiento { get; set; }
     }
 }
